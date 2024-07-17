f08c466b983205ceac58323eb449004de4c04fa6
bethmij <bethmij@gmail.com>
2024-07-17 14:51:37 +0530

    login page modified

--- SmartPay/SmartPay/app/screens/pages/Dashboard.tsx
+++ SmartPay/SmartPay/app/screens/pages/Dashboard.tsx
@@ -1,188 +1,189 @@
-import React from 'react'
-import { Image, View, StatusBar, TouchableOpacity } from 'react-native'
-import { Headline, Text } from 'react-native-paper'
-import { ScrollView } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useRouter } from 'expo-router';
+import React from 'react';
+import { View, Text, Image, ScrollView, TouchableOpacity, SafeAreaView } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-const bell = require('../../../assets/images/bell.png');
-const smartpaylogo = require('../../../assets/images/smartpay.png');
-const promotion = require('../../../assets/images/promotion.png');
 
-export default function Dashboard() {
-
-  const router = useRouter();
-
+const Dashboard = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View className='flex w-full h-full' >
-        <StatusBar barStyle="light-content" backgroundColor="#222222" />
-
-        <View className='h-1/3 w-full bg-[#222222]' >
-
-          <View className='flex flex-row items-center h-1/4 w-full mt-2' >
-
-            <View className='flex flex-row w-1/2 h-full'>
-              <View className='w-1/3 h-full flex justify-center items-center'>
-                <View className=' flex justify-between w-3/6 h-7'>
-                  <View className='w-2/3 h-1 bg-[#F1B041] rounded-full'></View>
-                  <View className='w-full h-1 bg-white rounded-full'></View>
-                  <View className='w-full h-1 bg-white rounded-full'></View>
-                </View>
-              </View>
-              <View className='flex flex-row items-center bg-[#2F2F2F] w-[33.5vw] h-full rounded-full'>
-                <View className=' relative left-2 w-[14vw] h-[14vw] rounded-full border-[#F1B041] border-[3px]'></View>
-                <View className=' relative left-3 w-14 h-5/6 justify-center'>
-                  <Text className='text-white ml-1'>Hello,</Text>
-                  <Text className='font-bold text-white text-lg ml-1'>Yasas</Text>
-                </View>
-              </View>
-            </View>
-
-            <View className='flex items-end w-1/2 h-full '>
-              <View className='flex items-center justify-center h-full w-1/3'>
-                <Image source={bell} className='h-10 w-10 rotate-45 ml-3'></Image>
-              </View>
-            </View>
+    <ScrollView className='bg-black'>
+      {/* Header */}
+      <SafeAreaView className="flex-row items-center justify-between p-4 pt-5 mt-5">
+          <Image source={require("../../../assets/wattpad-logo.png")} className="h-10 w-10" />
+          
+          <View className="flex-row items-center">
+            <TouchableOpacity className="bg-purple-400 p-2 flex flex-row rounded-full">
+              <Ionicons name="flash-outline" size={20} color="white" />
+              <Text className="text-white">Try Premium</Text>
+            </TouchableOpacity>         
+            <TouchableOpacity className="mx-2 p-1 bg-yellow-500 rounded-full">
+              <Ionicons name="gift-outline" size={24} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity className="mx-2">
+              <Ionicons name="settings-outline" size={24} color="white" />
+            </TouchableOpacity>
+            <Image source={require("../../../assets/profile-logo.jpg")} className="h-10 w-10 rounded-full" />
           </View>
+        </SafeAreaView>
 
-          <View className='flex justify-center items-center h-2/6 w-full' >
-            <Image source={smartpaylogo} className='h-20 w-44 relative bottom-1'></Image>
+      {/* Banner */}
+      <View className="p-4">
+        <Text className="text-white text-2xl">Out now: Heart of Iron ❤️‍🔥</Text>
+        <Text className="text-gray-400 text-lg font-bold ">A world of secrets and danger</Text>
+        <Text className="text-gray-400 mb-3">Will Rowan, an outsider, find love as Casimir... Thorn's mate?</Text>
+        <Image source={require('../../../assets/banner.png') } className="h-40 w-full mt-2 rounded" />
+      </View>
+
+      {/* Your Stories */}
+      <View className="p-4 bg-[#01170A] border-y-2 border-green-900">
+        <Text className="text-white text-xl font-bold">Your stories</Text>
+        <Text className="text-gray-400 mb-5">Catch up on the stories you love!</Text>
+        <ScrollView horizontal>
+          <View className="mr-4">
+            <Image source={require('../../../assets/yourBooks/story1.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Continue reading</Text>
+            <Text className="text-green-400 text-right">3 new parts</Text>
+            <Text className="text-gray-600 text-right text-xs">2d ago</Text>
           </View>
-
-          <View className='flex flex-row items-center justify-around h-3/6 w-full bg-[#222222]' >
-            <View className='w-[90vw] h-full flex flex-row items-center justify-around'>
-              <View className='flex justify-center items-center'>
-                <TouchableOpacity onPress={() => { router.navigate("screens/pages/FundTransfer") }} >
-                  <View className={"bg-[#F1B041] w-[16vw] h-[16vw] rounded-full relative bottom-3"}></View>
-                </TouchableOpacity>
-                <Text className='relative bottom-2 text-white'>Fund Tranfer</Text>
-              </View>
-
-              <View className='flex justify-center items-center'>
-                <View className={"bg-[#F1B041] w-[16vw] h-[16vw] rounded-full relative bottom-3"}></View>
-                <Text className='relative bottom-2 text-white'>QR Payment</Text>
-              </View>
-
-              <View className='flex justify-center items-center'>
-                <TouchableOpacity onPress={() => { router.navigate("screens/pages/BillPay") }} >
-                  <View className={"bg-[#F1B041] w-[16vw] h-[16vw] rounded-full relative bottom-3"}></View>
-                </TouchableOpacity>
-                <Text className='relative bottom-2 text-white'>Pay Bils</Text>
-              </View>
-
-              <View className='flex justify-center items-center'>
-                <TouchableOpacity onPress={() => { router.navigate("screens/pages/Business") }} >
-                  <View className={"bg-[#F1B041] w-[16vw] h-[16vw] rounded-full relative bottom-3"}></View>
-                </TouchableOpacity>
-                <Text className='relative bottom-2 text-white'>My Shop</Text>
-              </View>
-
-            </View>
-
+          
+          <View className="mr-4">
+            <Image source={require('../../../assets/yourBooks/story2.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Updated recently</Text>
+            <Text className="text-green-400 text-right">3 new parts</Text>
+            <Text className="text-gray-600 text-right text-xs">10h ago</Text>
           </View>
+          <View className="mr-4">
+            <Image source={require('../../../assets/yourBooks/story3.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Updated recently</Text>
+            <Text className="text-green-400 text-right">6 new parts</Text>
+            <Text className="text-gray-600 text-right text-xs">1d ago</Text>
+          </View>
+          <View className="mr-4">
+            <Image source={require('../../../assets/yourBooks/story4.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Psycho Love</Text>
+            <Text className="text-green-400 text-right">2 new parts</Text>
+            <Text className="text-gray-600 text-right text-xs">5d ago</Text>
+          </View>
+        </ScrollView>
+      </View>
 
+      {/* Top Picks For You */}
+      <View className="p-4">
+        <Text className="text-white text-xl font-bold mb-5">Top Picks For You</Text>
+        <ScrollView horizontal>
+          <View className="mr-4">
+            <Image source={require('../../../assets/topPicks/pick1.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Broken Mirrors</Text>
+          </View>
+          <View className="mr-4">
+            <Image source={require('../../../assets/topPicks/pick2.jpeg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Sepalika</Text>
+          </View>
+          <View className="mr-4">
+            <Image source={require('../../../assets/topPicks/pick3.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Camelia</Text>
+          </View>
+          <View className="mr-4">
+            <Image source={require('../../../assets/topPicks/pick4.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Sidharana</Text>
+          </View>
+        </ScrollView>
+      </View>
+
+      <SafeAreaView className="bg-black p-4 rounded-lg shadow-lg max-w-full flex mt-5">
+      <Image
+        source={require('../../../assets/book1.jpg' )} 
+        className=" h-48 rounded-lg"
+        resizeMode="cover"
+      />
+      <View className="mt-4 w-full">
+        <View className='flex flex-row justify-between'>       
+          <View>
+            <Text className="text-white text-xl font-bold">Darkness</Text>
+            <Text className="text-gray-400 text-sm mt-1">37 Parts</Text>
+          </View>
+          
+          <Text className="bg-yellow-500 text-black px-2 py-1 rounded-full w-auto mt-1 h-8">Ongoing</Text>
         </View>
 
-
-        <View className='flex w-full h-2/3 top-4'>
-          <ScrollView className='w-full'>
-
-            <View className='flex justify-center items-center h-[67vw] w-full bg-white '>
-
-              <View className='h-[12vw] w-full flex-row items-center justify-between px-4'>
-                <Headline className='text-lg font-bold pl-1'>Promotions</Headline>
-                <Text className='text-[#e7a93e] font-bold'>View All</Text>
-              </View>
-              <Image source={promotion} className='h-[55vw] w-[91vw]'></Image>
-            </View>
-
-            <View className='flex items-center h-[35vw] w-full bg-white '>
-
-              <View className='h-[12vw] w-full flex-row items-center justify-between px-4'>
-                <Headline className='text-lg font-bold pl-1'>Accounts</Headline>
-                <Text className='text-[#e7a93e] font-bold'>Manage</Text>
-              </View>
-              <View className='flex-row items-center justify-between px-4 h-[23vw] w-[91vw] rounded-xl 
-          border-l-[2vw] border-l-[#F1B041] border-r-[0.8px] border-t-[0.8px] border-b-[0.8px]
-          border-t-gray-500 border-b-gray-500 border-r-gray-500'>
-
-                <View className='flex justify-center w-1/2 h-2/3'>
-                  <Headline className='text-lg font-bold pl-1 m-0'>MY BOC</Headline>
-                  <Headline className='text-lg font-bold pl-1 m-0'>XXXX</Headline>
-                </View>
-
-                <View className='flex items-end justify-center w-1/2 h-2/3 '>
-                  <Text className='text-lg font-bold pl-1 m-0'>Balance</Text>
-                  <View className='flex justify-center items-center flex-row '>
-                    <Text className='text-sm font-bold pl-1 m-0'>LKR</Text>
-                    <Text className='text-lg font-bold pl-1 m-0'>100,000.00</Text>
-                  </View>
-
-                </View>
-              </View>
-            </View>
-
-            <View className='flex items-center h-auto w-full bg-white mb-3'>
-
-              <View className='h-[12vw] w-full flex-row items-center justify-between px-4'>
-                <Headline className='text-lg font-bold pl-1'>Recent Transactions</Headline>
-                <Text className='text-[#e7a93e] font-bold'>View All</Text>
-              </View>
-              <View className='flex-row items-center justify-between mb-3 px-4 h-[23vw] w-[91vw] rounded-xl border-[0.6px] bg-[#EDEDED]'>
-
-                <View className='flex justify-center w-1/2 h-2/3'>
-                  <Headline className='text-lg font-bold pl-1 m-0 '>Bill Payment</Headline>
-                  <Text className='text-lg pl-1 m-0'>Hutch</Text>
-                </View>
-
-                <View className='flex items-end justify-center w-1/2 h-2/3 '>
-                  <View className='flex justify-center items-center flex-row '>
-                    <Text className='text-sm font-bold pl-1 m-0'>LKR</Text>
-                    <Text className='text-lg font-bold pl-1 m-0'>100.00</Text>
-                  </View>
-                  <Text className='text-base pl-1 m-0'>04-jul-24 11.06AM</Text>
-                </View>
-              </View>
-
-              <View className='flex-row items-center justify-between mb-3 px-4 h-[23vw] w-[91vw] rounded-xl border-[0.6px] bg-[#EDEDED]'>
-
-                <View className='flex justify-center w-1/2 h-2/3'>
-                  <Headline className='text-lg font-bold pl-1 m-0 '>Bill Payment</Headline>
-                  <Text className='text-lg pl-1 m-0'>Dialog</Text>
-                </View>
-
-                <View className='flex items-end justify-center w-1/2 h-2/3 '>
-                  <View className='flex justify-center items-center flex-row '>
-                    <Text className='text-sm font-bold pl-1 m-0'>LKR</Text>
-                    <Text className='text-lg font-bold pl-1 m-0'>200.00</Text>
-                  </View>
-                  <Text className='text-base pl-1 m-0'>04-jul-24 11.06AM</Text>
-                </View>
-              </View>
-
-              <View className='flex-row items-center justify-between mb-3 px-4 h-[23vw] w-[91vw] rounded-xl border-[0.6px] bg-[#EDEDED]'>
-
-                <View className='flex justify-center w-1/2 h-2/3'>
-                  <Headline className='text-lg font-bold pl-1 m-0 '>Bill Payment</Headline>
-                  <Text className='text-lg pl-1 m-0'>Hutch</Text>
-                </View>
-
-                <View className='flex items-end justify-center w-1/2 h-2/3 '>
-                  <View className='flex justify-center items-center flex-row '>
-                    <Text className='text-sm font-bold pl-1 m-0'>LKR</Text>
-                    <Text className='text-lg font-bold pl-1 m-0'>100.00</Text>
-                  </View>
-                  <Text className='text-base pl-1 m-0'>04-jul-24 11.06AM</Text>
-                </View>
-              </View>
-
-            </View>
-
-          </ScrollView>
+       
+        <Text className="text-white mt-2">
+          When Emery Jones stumbles upon Damon Cavanaugh's hidden dark secret, she escapes to Europe with his sworn rival, Quinton Marquis, seeking solace in what was meant to be a temporary escape...
+        </Text>
+        <View className="flex-row justify-between items-center mt-4 mb-5">
+          <TouchableOpacity className="bg-white text-black px-4 py-2 rounded-full">
+            <Text className="font-semibold">Read now</Text>
+          </TouchableOpacity>
         </View>
-
+      
       </View>
     </SafeAreaView>
-  )
-}
+
+      {/* Wattpad Originals */}
+      <View className="p-4 bg-gray-900 border-y-2 border-gray-700">
+        <Text className="text-white text-xl font-bold mb-5">Wattpad Originals for Bjbrooke</Text>
+        <ScrollView horizontal>
+          <View className="mr-4">
+            <Image source={require('../../../assets/originals/original1.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">House of Cards</Text>
+          </View>
+          <View className="mr-4">
+            <Image source={require('../../../assets/originals/original2.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Fifty Shades</Text>
+          </View>
+          <View className="mr-4">
+            <Image source={require('../../../assets/originals/original3.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Force Marriage</Text>
+          </View>
+          <View className="mr-4">
+            <Image source={require('../../../assets/originals/original4.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Mafia's Queen</Text>
+          </View>
+        </ScrollView>
+      </View>
+
+      {/* Based on genres you love */}
+      <View className="p-4">
+        <Text className="text-white text-xl font-bold mb-5">Based on genres you love</Text>
+        <ScrollView horizontal>
+          <View className="mr-4">
+            <Image source={require('../../../assets/genre/genre1.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Numba Rithuwak</Text>
+          </View>
+          <View className="mr-4">
+            <Image source={require('../../../assets/genre/genre2.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Niwalanna Ma</Text>
+          </View>
+          <View className="mr-4">
+            <Image source={require('../../../assets/genre/genre3.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Kemura</Text>
+          </View>
+          <View className="mr-4">
+            <Image source={require('../../../assets/genre/genre4.jpg')} className="h-40 w-24 rounded" />
+            <Text className="text-white mt-2">Dehadaka Kadulu</Text>
+          </View>         
+        </ScrollView>
+      </View>
+
+      {/* Footer */}
+      <SafeAreaView className="bottom-0 w-full flex-row items-center justify-around p-4 bg-black">
+        <TouchableOpacity>
+          <Ionicons name="home-outline" size={24} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons name="search-outline" size={24} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons name="pencil-outline" size={24} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons name="notifications-outline" size={24} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons name="person-outline" size={24} color="white" />
+        </TouchableOpacity>
+      </SafeAreaView>
+    </ScrollView>
+  );
+};
+
+export default Dashboard;
--- SmartPay/SmartPay/tailwind.config.js
+++ SmartPay/SmartPay/tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      textShadow: {
+        'default': '0 1px 3px rgba(0, 0, 0, 0.1)',
+        'md': '0 3px 6px rgba(0, 0, 0, 0.1)',
+        'lg': '0 10px 20px rgba(0, 0, 0, 0.1)',
+        'xl': '0 20px 40px rgba(0, 0, 0, 0.1)',
+        '2xl': '0 25px 50px rgba(0, 0, 0, 0.25)',
+        'none': 'none',
+      }
+    },
+  },  
   plugins: [],
 }
 
