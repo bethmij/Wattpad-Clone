47f890dd2fd6f0efc5b515f04404850ea0d9f090
bethmij <bethmij@gmail.com>
2024-07-17 14:02:10 +0530

    dashboard modified

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Index from "./app/index"
+import { View } from 'react-native'
+export default function App() {
+  return (
+        <View className='flex-1 w-ful h-full bg-slate-600'></View>
+  )
+}
